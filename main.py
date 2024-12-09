c6f55d0bf272d2f64e83e92c1a332ac81f849163
h.yamamori <yamaharu0205@gmail.com>
2024-12-09 13:07:09 +0900

    fix prompt

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ gemini_pro = genai.GenerativeModel("gemini-1.5-flash",
 
 
 def generate_math_problems_json(genre, difficulty, num_problems):
-    prompt = f"数学の問題と解答例を{num_problems}個作成してください。単元は{genre}で、難易度は{difficulty}です。解説は詳細に記載してください。解答は、問題文、解答、解説のキーを持つJSONオブジェクトとして出力してください。"
+    prompt = f"数学の問題と解答例を{num_problems}個作成してください。単元は{genre}で、難易度は{difficulty}です。解説は、公式の利用、計算過程、解答に至るまでの思考過程などを含めて記述してください。解答は、問題文、解答、解説のキーを持つJSONオブジェクトとして出力し、JSONオブジェクトのキーは、\"problem\", \"answer\", \"explanation\"としてください。"
     print(f"プロンプト：{prompt}")
     response = gemini_pro.generate_content(prompt)
     print(response.text)
@@ -31,7 +31,7 @@ def generate_math_problems_json(genre, difficulty, num_problems):
 
 if __name__ == "__main__":
     genre = "二次関数"
-    difficulty = "上級"
+    difficulty = "高校1年生レベル"
     num_problems = 5
     # 引数は「単元」「レベル」「問題数」を自由に設定
     json_result = generate_math_problems_json(genre, difficulty, num_problems)
